c0545f7e68f79653c4552d626bfd5dc86c8f7c44
TAREKHAVRYLIUK <131817804+TAREKHAVRYLIUK@users.noreply.github.com>
2025-05-11 18:22:12 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import App from './App';
+import './index';
 
 const rootElement = document.getElementById('root');
 
